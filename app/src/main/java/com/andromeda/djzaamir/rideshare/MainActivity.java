f8e26cf05c8e20bd86f8ced8d06993d1b744d8cb
djzaamir <djzaamir@gmail.com>
2018-04-22 23:17:56 +0500

    Welcome Activity added, work on GUI underway, also added a startup activity to display the Logo of Rideshare

--- app/src/main/java/com/andromeda/djzaamir/rideshare/MainActivity.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andromeda.djzaamir.rideshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                long sleep_for = 1500L;
+
+                try {
+                    //Sleep for S seconds and then start Welcome Login/Signup activity
+                    Thread.sleep(sleep_for);
+
+                    Intent welcomeActivityIntent = new Intent(MainActivity.this,WelcomeActivity.class);
+                    startActivity(welcomeActivityIntent);
+
+                    //Dispose off current activity
+                    finish();
+                    return;
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.andromeda.djzaamir.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void openLoginActivity(View view) {
+    }
+
+    public void openSignupActivity(View view) {
+    }
+}
