be579e6016bb2c885c15d790a1c8cd9ced434d31
djzaamir <djzaamir@gmail.com>
2018-04-28 00:20:42 +0500

    Changes made to dev_notes and TODO list

--- app/src/main/java/com/andromeda/djzaamir/rideshare/NavigationDrawer.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/NavigationDrawer.java
@@ -1,12 +1,7 @@
 package com.andromeda.djzaamir.rideshare;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
