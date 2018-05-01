fd3ddbf2f0ffe196a1c5101c1a906ad7bcc898ad
djzaamir <djzaamir@gmail.com>
2018-05-01 14:48:23 +0500

    Share my ride Acitivty UnderDevelopment, gui boiler plate ready, now working to enable its components and connect Google Maps API and Google Places Api

--- app/src/main/java/com/andromeda/djzaamir/rideshare/shareMyRide.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/shareMyRide.java
@@ -2,6 +2,7 @@ package com.andromeda.djzaamir.rideshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class shareMyRide extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class shareMyRide extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_my_ride);
+    }
+
+    public void date_time_onClick(View view) {
     }
 }
