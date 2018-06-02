6916c8ac82ad39c239a6ceadb23d47cacc10d627
djzaamir <djzaamir@gmail.com>
2018-06-02 14:06:36 +0500

    Started Working on ShowDriverDetails Activity

---
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/DisplayDriverDetails.java
@@ -0,0 +1,13 @@
+package com.andromeda.djzaamir.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayDriverDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_driver_details);
+    }
+}
