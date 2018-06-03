f9e0a857c36d6ecc0c03cacb78b6ee121cdc90f6
djzaamir <djzaamir@gmail.com>
2018-06-03 16:10:56 +0500

    WorkUnderway on GUI , added pickup and destination address

--- app/src/main/java/com/andromeda/djzaamir/rideshare/DisplayDriverDetails.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/DisplayDriverDetails.java
@@ -1,6 +1,8 @@
 package com.andromeda.djzaamir.rideshare;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,10 +14,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class DisplayDriverDetails extends AppCompatActivity {
 
     private ImageView driver_image_imageview;
-    private TextView driver_name_textview;
+    private TextView driver_name_textview , driver_pickup_address , driver_destination_address;
     private String u_id;
 
     @Override
@@ -24,15 +30,17 @@ public class DisplayDriverDetails extends AppCompatActivity {
         setContentView(R.layout.activity_display_driver_details);
 
         //Establish references with gui
-        driver_image_imageview = findViewById(R.id.driver_image_imageview);
-        driver_name_textview =  findViewById(R.id.driver_name);
+        driver_image_imageview     =  findViewById(R.id.driver_image_imageview);
+        driver_name_textview       =  findViewById(R.id.driver_name);
+        driver_pickup_address      =  findViewById(R.id.driver_pickup_address);
+        driver_destination_address =  findViewById(R.id.driver_destination_address);
 
         //grab id from intent
         Intent  intent_data = getIntent();
         u_id =  intent_data.getStringExtra("id");
 
         //grab Driver Picture from firebase
-        FirebaseDatabase.getInstance().getReference().child("Users").child(u_id).addValueEventListener(new                                      ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference().child("Users").child(u_id).addValueEventListener(new                                                      ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot != null &&  dataSnapshot.getValue() != null){
@@ -55,7 +63,56 @@ public class DisplayDriverDetails extends AppCompatActivity {
 
 
 
-        //grab other details
+        //grab Driver Pickup and drop-off loction
+        FirebaseDatabase.getInstance().getReference().child("available_drivers_start_point").child(u_id).child("l").addValueEventListener(new                   ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null && dataSnapshot.getValue() != null){
+                    String lat =  dataSnapshot.child("0").getValue().toString();
+                    String lng =  dataSnapshot.child("1").getValue().toString();
+                    Geocoder decodeLatLng =  new Geocoder(getApplicationContext(), Locale.getDefault());
+                    try {
+                        List<Address> addresses =  decodeLatLng.getFromLocation(Double.parseDouble(lat) , Double.parseDouble(lng) , 1);
+
+                        //set address to text field
+                        driver_pickup_address.setText(addresses.get(0).getAddressLine(0).toString());
+                        driver_pickup_address.setSelected(true);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        FirebaseDatabase.getInstance().getReference().child("available_drivers_end_point").child(u_id).child("l").addValueEventListener(new                   ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null && dataSnapshot.getValue() != null){
+                    String lat =  dataSnapshot.child("0").getValue().toString();
+                    String lng =  dataSnapshot.child("1").getValue().toString();
+                    Geocoder decodeLatLng =  new Geocoder(getApplicationContext(), Locale.getDefault());
+                    try {
+                        List<Address> addresses =  decodeLatLng.getFromLocation(Double.parseDouble(lat) , Double.parseDouble(lng) , 1);
+
+                        //set address to text field
+                        driver_destination_address.setText(addresses.get(0).getAddressLine(0).toString());
+                        driver_destination_address.setSelected(true);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
