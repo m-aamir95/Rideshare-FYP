53d814e5c5e9188b7b6591bee3b69616de7e82ac
djzaamir <djzaamir@gmail.com>
2018-06-10 17:53:53 +0500

    Added app logo, Welcome Screen Logo, And Login/Signup Screen Logo

--- app/src/main/java/com/andromeda/djzaamir/rideshare/ChatActivity.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/ChatActivity.java
@@ -2,25 +2,72 @@ package com.andromeda.djzaamir.rideshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.andromeda.djzaamir.rideshare.utils.InputUtils;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
 
     private String driver_id , u_id;
-
+    private ImageView driver_image,send_button;
+    private TextView driver_name;
+    private EditText chat_message_Edittextview;
+    private Button ride_request_button;
+    private LinearLayout chats_container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        //Get Gui references
+        driver_image = findViewById(R.id.driver_image);
+        send_button =  findViewById(R.id.send_msg_button_imageview);
+        driver_name =  findViewById(R.id.driver_name);
+        ride_request_button =  findViewById(R.id.ride_request_button);
+        chat_message_Edittextview =  findViewById(R.id.chat_message_edittextview);
+
+
+
+
 
         //Grab driver id from intent
         driver_id =  getIntent().getStringExtra("driver_id");
         u_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    //Send button/Image for chats
+    public void send_chat_message(View view) {
+
+        if (chat_message_Edittextview.getText().toString().trim().length() > 0 == false){
+            return; //Quite Method
+        }
+
+        //Disable msg button for timebeing
+        InputUtils.disableInputControls(send_button);
 
 
+        //Grab and send message to firebase
+        String msg =  chat_message_Edittextview.getText().toString();
 
+        //Every chat has a unique id which is comprised of   chat_id = driver_id + customer_id;
+        DatabaseReference chat_node = FirebaseDatabase.getInstance().getReference().child(driver_id + u_id);
+        /*
+        * Push will make a random ID entry inside this node
+        * Against this random ID we will insert this new Chat Message
+        * */
+        chat_node.push().setValue(msg);
+
+
+        //Enable button
+        InputUtils.enableInputControls();
     }
 }
