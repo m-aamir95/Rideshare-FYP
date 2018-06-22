0f81a0ef866316e5595b201f517beaf2dd031a3b
djzaamir <djzaamir@gmail.com>
2018-06-22 13:41:10 +0500

    Total Distance , and Estimated Fair Is now being shown at each details Activity Screen

--- app/src/main/java/com/andromeda/djzaamir/rideshare/DisplayDriverDetails.java
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/DisplayDriverDetails.java
@@ -3,32 +3,41 @@ package com.andromeda.djzaamir.rideshare;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.andromeda.djzaamir.rideshare.AdsManager.AdManager;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.internal.InternalTokenProvider;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
 public class DisplayDriverDetails extends AppCompatActivity {
 
+    //region VARS
     private ImageView driver_image_imageview;
     private TextView driver_name_textview , driver_pickup_address , driver_destination_address,
-                     driver_pickup_time,driver_return_time;
+                     driver_pickup_time,driver_return_time , distance_textview, fair_textview;
     private String u_id;
+
+    private LatLng driver_start_point = null, driver_end_point = null;
+    private double journey_Distance;
+    private boolean journey_Distance_set = false;
+    //endregion
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,8 @@ public class DisplayDriverDetails extends AppCompatActivity {
         driver_destination_address =  findViewById(R.id.driver_destination_address);
         driver_pickup_time         =  findViewById(R.id.driver_pickup_time);
         driver_return_time         =  findViewById(R.id.driver_return_time);
+        distance_textview          =  findViewById(R.id.distance_textView);
+        fair_textview              =  findViewById(R.id.fair_textView);
 
         //grab id from intent
         Intent  intent_data = getIntent();
@@ -78,9 +89,14 @@ public class DisplayDriverDetails extends AppCompatActivity {
                 if (dataSnapshot != null && dataSnapshot.getValue() != null){
                     String lat =  dataSnapshot.child("0").getValue().toString();
                     String lng =  dataSnapshot.child("1").getValue().toString();
+
+                    driver_start_point = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+                    calcAndUpdateDistance();
+                    calcAndUpdateFair();
+
                     Geocoder decodeLatLng =  new Geocoder(getApplicationContext(), Locale.getDefault());
                     try {
-                        List<Address> addresses =  decodeLatLng.getFromLocation(Double.parseDouble(lat) , Double.parseDouble(lng) , 1);
+                     List<Address> addresses =  decodeLatLng.getFromLocation(Double.parseDouble(lat) , Double.parseDouble(lng) , 1);
 
                         //set address to text field
                         driver_pickup_address.setText(addresses.get(0).getAddressLine(0).toString());
@@ -103,6 +119,11 @@ public class DisplayDriverDetails extends AppCompatActivity {
                 if (dataSnapshot != null && dataSnapshot.getValue() != null){
                     String lat =  dataSnapshot.child("0").getValue().toString();
                     String lng =  dataSnapshot.child("1").getValue().toString();
+
+                    driver_end_point = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+                    calcAndUpdateDistance();
+                    calcAndUpdateFair();
+
                     Geocoder decodeLatLng =  new Geocoder(getApplicationContext(), Locale.getDefault());
                     try {
                         List<Address> addresses =  decodeLatLng.getFromLocation(Double.parseDouble(lat) , Double.parseDouble(lng) , 1);
@@ -158,6 +179,39 @@ public class DisplayDriverDetails extends AppCompatActivity {
 
     }
 
+    private void calcAndUpdateFair() {
+      if (journey_Distance_set){
+        int fair_per_km = 10;
+
+        //Distance fair
+        double calculated_fair = fair_per_km * journey_Distance;
+
+        //Base Fair
+        calculated_fair += 20;
+
+        String formatted_fair = new DecimalFormat("#0").format(calculated_fair);
+
+        fair_textview.setText("Fair: " + formatted_fair + " Rs");
+      }
+    }
+
+    private void calcAndUpdateDistance() {
+        if (driver_start_point != null && driver_end_point != null){
+
+            //enable fair calculation
+            journey_Distance_set = true;
+
+           journey_Distance = getDistance(driver_start_point ,  driver_end_point);
+
+           journey_Distance /= 1000; //km conversion
+
+           //Perform Decimal Formatting before sending to display
+           String formatted_distance = new DecimalFormat("#0.0").format(journey_Distance);
+
+           distance_textview.setText("Distance: " + formatted_distance + " km");
+        }
+    }
+
     private void parseTimestampAndSetupTextView(long timestamp, TextView target_textview){
         Calendar c =  Calendar.getInstance();
         c.setTimeInMillis(timestamp);
@@ -202,5 +256,35 @@ public class DisplayDriverDetails extends AppCompatActivity {
             startActivity(chatActivityIntent);
 
         }
+    }
+
+
+
+    //Calculate Distance between two points On earth using Haversine formula
+    double degreeToRad(double degrees){
+        return degrees * Math.PI / 180;
+    }
+    /*
+    * Here we can consider point_A as Starting Point of the Journey
+    * And point_B as the Ending point of the journey
+    * */
+    double getDistance(LatLng  point_A ,  LatLng point_B){
+        double R = 6378137; // Earth’s mean radius in meter;
+
+        //Subtract Both Distance's from each other And Convert them to Radians
+        double dLat =  degreeToRad(point_B.latitude  - point_A.latitude);
+        double dLng =  degreeToRad(point_B.longitude - point_A.longitude);
+
+        //Some weired mathematics
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                   Math.cos(degreeToRad(point_A.latitude)) * Math.cos(degreeToRad(point_B.latitude)) *
+                   Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+        //More Weired mathematics
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+
+        double d = R * c;
+        return d;
     }
 }
