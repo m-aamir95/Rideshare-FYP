3a0875c1ac3bad2ba3b57d699e43ea3f8381bce1
djzaamir <djzaamir@gmail.com>
2018-07-18 13:12:09 +0500

    ChangeSettingsActivity, to allow  changing different user settings is GUI ready by coying careem Styles

---
+++ app/src/main/java/com/andromeda/djzaamir/rideshare/ChangeSettingsActivity.java
@@ -0,0 +1,17 @@
+package com.andromeda.djzaamir.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_settings);
+
+
+
+
+    }
+}
